e3f4bf51173fa7b90bbc3620db5585f8b51352fd
rasmus-kirk <mail@rasmuskirk.com>
2023-05-26 18:57:53 +0200

    Fixup

--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "Kirk nix modules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #home-manager = {
+    #  url = "github:nix-community/home-manager";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
   };
 
   outputs = {
     self,
-    nixpkgs,
-    home-manager,
+    #nixpkgs,
+    #home-manager,
   }: {
     nixosModules.kirk = import ./nixos;
     nixosModules.default = self.nixosModules.kirk;
 
     homeManagerModules.kirk = import ./home-manager;
-    homeManagerModules.default = self.homeManagerModules.age;
+    homeManagerModules.default = self.homeManagerModules.kirk;
 
     # Work-around for https://github.com/nix-community/home-manager/issues/3075
     #legacyPackages = nixpkgs.lib.genAttrs ["aarch64-darwin" "x86_64-darwin"] (system: {
