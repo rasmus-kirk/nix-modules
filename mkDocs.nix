2f8bec6daf8b51b4d93cb910d692498f0936f6a1
Brian McGee <brian@bmcgee.ie>
2023-05-28 13:58:33 +0100

    feat: convert to flake and fix mkdocs

--- mkDocs.nix
+++ mkDocs.nix
@@ -1,32 +1,44 @@
-{ lib, pkgs, nixosOptionsDoc, ...}:
-    let
-    # evaluate our options
-    eval = lib.evalModules {
-        check = false;
-        modules = [
-            ./home-manager/fonts
-            ./home-manager/foot
-            ./home-manager/fzf
-            ./home-manager/git
-            ./home-manager/gruvboxTheme
-            ./home-manager/helix
-            ./home-manager/homeManagerScripts
-            ./home-manager/jiten
-            ./home-manager/joshuto
-            ./home-manager/kakoune
-            ./home-manager/ssh
-            ./home-manager/terminalTools
-            ./home-manager/userDirs
-            ./home-manager/zathura
-            ./home-manager/zsh
-        ];
-    };
-    # generate our docs
-    optionsDoc = nixosOptionsDoc {
-        inherit (eval) options;
-    };
+{
+  lib,
+  runCommand,
+  nixosOptionsDoc,
+  pkgs,
+  ...
+}: let
+  # Make sure the used package is scrubbed to avoid actually
+  # instantiating derivations.
+  # evaluate our options
+  eval = lib.evalModules {
+    # todo understand why pkgs needs to be passed here
+    specialArgs = {inherit pkgs;};
+    modules = [
+      {
+        # disabled checking that all option definitions have matching declarations
+        config._module.check = false;
+      }
+      ./home-manager/fonts
+      ./home-manager/foot
+      ./home-manager/fzf
+      ./home-manager/git
+      ./home-manager/gruvboxTheme
+      ./home-manager/helix
+      ./home-manager/homeManagerScripts
+      ./home-manager/jiten
+      ./home-manager/joshuto
+      ./home-manager/kakoune
+      ./home-manager/ssh
+      ./home-manager/terminalTools
+      ./home-manager/userDirs
+      ./home-manager/zathura
+      ./home-manager/zsh
+    ];
+  };
+  # generate our docs
+  optionsDoc = nixosOptionsDoc {
+    inherit (eval) options;
+  };
 in
-    # create a derivation for capturing the markdown output
-    pkgs.runCommand "options-doc.md" {} ''
-        cat ${optionsDoc.optionsCommonMark} >> $out
-    ''
+  # create a derivation for capturing the markdown output
+  runCommand "options-doc.md" {} ''
+    cat ${optionsDoc.optionsCommonMark} >> $out
+  ''
--- nixos/default.nix
+++ nixos/default.nix
@@ -1,5 +1,5 @@
 {
-	imports = [
-		./openssh
-	]; 
+  imports = [
+    ./openssh
+  ];
 }
