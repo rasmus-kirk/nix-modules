41edb43b749dbd415832e9cf8c981b671696cb87
rasmus-kirk <mail@rasmuskirk.com>
2023-12-28 10:51:27 +0100

    Fixed upnp firewall error

--- nixos/servarr/default.nix
+++ nixos/servarr/default.nix
@@ -322,13 +322,13 @@ in {
     # TODO: Understand this properly
     networking.firewall.extraCommands = ''
       # Rules for IPv4:
-      ${pkgs.ipset}/bin/ipset create upnp hash:ip,port timeout 3
+      ${pkgs.ipset}/bin/ipset -exist create upnp hash:ip,port timeout 3
       iptables -A OUTPUT -d 239.255.255.250/32 -p udp -m udp --dport 1900 -j SET --add-set upnp src,src --exist
       iptables -A INPUT -p udp -m set --match-set upnp dst,dst -j ACCEPT
       iptables -A INPUT -d 239.255.255.250/32 -p udp -m udp --dport 1900 -j ACCEPT
 
       # Rules for IPv6:
-      ${pkgs.ipset}/bin/ipset create upnp6 hash:ip,port timeout 3 family inet6
+      ${pkgs.ipset}/bin/ipset -exist create upnp6 hash:ip,port timeout 3 family inet6
       ip6tables -A OUTPUT -d ff02::c/128 -p udp -m udp --dport 1900 -j SET --add-set upnp6 src,src --exist
       ip6tables -A OUTPUT -d ff05::c/128 -p udp -m udp --dport 1900 -j SET --add-set upnp6 src,src --exist
       ip6tables -A INPUT -p udp -m set --match-set upnp6 dst,dst -j ACCEPT
