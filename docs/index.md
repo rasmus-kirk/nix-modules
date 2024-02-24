e82851e9a0c474f9c64e870de7491de2d118593e
rasmus-kirk <mail@rasmuskirk.com>
2024-02-24 14:24:35 +0100

    updated docs process

---
+++ docs/index.md
@@ -0,0 +1,4 @@
+This is the options documentation for my personal nix modules
+
+- **Documentation for the nixos modules** can be found [here](./nixos.html)
+- **Documentation for the home manager modules** can be found [here](./home.html)
--- mkDocs.nix
+++ mkDocs.nix
@@ -1,7 +1,6 @@
 {
   lib,
   pkgs,
-  runCommand,
   nixosOptionsDoc,
   inputs,
   ...
@@ -18,21 +17,21 @@
         config._module.check = false;
       }
       inputs.home-manager.nixosModules.default
-      ./home-manager/fonts
-      ./home-manager/foot
-      ./home-manager/fzf
-      ./home-manager/git
-      ./home-manager/gruvboxTheme
-      ./home-manager/helix
-      ./home-manager/homeManagerScripts
-      ./home-manager/jiten
-      ./home-manager/joshuto
-      ./home-manager/kakoune
-      ./home-manager/ssh
-      ./home-manager/terminalTools
-      ./home-manager/userDirs
-      ./home-manager/zathura
-      ./home-manager/zsh
+      ./home-manager
+      #./home-manager/foot
+      #./home-manager/fzf
+      #./home-manager/git
+      #./home-manager/gruvboxTheme
+      #./home-manager/helix
+      #./home-manager/homeManagerScripts
+      #./home-manager/jiten
+      #./home-manager/joshuto
+      #./home-manager/kakoune
+      #./home-manager/ssh
+      #./home-manager/terminalTools
+      #./home-manager/userDirs
+      #./home-manager/zathura
+      #./home-manager/zsh
     ];
   };
   # generate our docs
@@ -55,11 +54,85 @@
   optionsDocNixos = nixosOptionsDoc {
     inherit (evalNixos) options;
   };
+in pkgs.stdenv.mkDerivation {
+    name = "nixdocs2html";
+    src = ./.;
+    buildInputs = with pkgs; [ pandoc ];
+    phases = ["unpackPhase" "buildPhase"];
+    buildPhase = ''
+      tmpdir=$(mktemp -d)
+      mkdir -p $out
+      cp docs/styling/style.css $out
 
-in
-  # create a derivation for capturing the markdown output
-  runCommand "options-doc.md" {} ''
-    mkdir -p $out
-    cat ${optionsDocHome.optionsCommonMark} | tail -n +210 >> $out/home.md
-    cat ${optionsDocNixos.optionsCommonMark} | tail -n +210 >> $out/nixos.md
-  ''
+      buildpandoc () {
+        file_path="$1"
+        filename=$(basename -- "$file_path")
+        filename_no_ext="''${filename%.*}"
+        echo "$file_path"
+        echo "$1"
+
+        # Remove "Declared by" lines
+        sed '/\*Declared by:\*/{N;d;}' "$file_path" > "$tmpdir"/"$filename_no_ext"1.md
+
+        echo 2
+
+        # Code blocks to nix code blocks
+        # shellcheck disable=SC2016
+        awk '
+        /^```$/ {
+            if (!block) {
+                print "```nix";  # Start of a code block
+                block = 1;
+            } else {
+                print "```";  # End of a code block
+                block = 0;
+            }
+            next;
+        }
+        { print }  # Print all lines, including those inside code blocks
+        ' block=0 "$tmpdir"/"$filename_no_ext"1.md > "$tmpdir"/"$filename_no_ext"2.md
+        # inline code "blocks" to nix code blocks
+        # shellcheck disable=SC2016
+        sed '/^`[^`]*`$/s/`\(.*\)`/```nix\n\1\n```/g' "$tmpdir"/"$filename_no_ext"2.md > "$tmpdir"/"$filename_no_ext"3.md
+        echo 3
+        # Remove bottom util-nixarr options
+        sed '/util-nixarr/,$d' "$tmpdir"/"$filename_no_ext"3.md > "$tmpdir"/done.md
+        echo 4
+
+        pandoc \
+          --standalone \
+          --highlight-style docs/styling/gruvbox.theme \
+          --metadata title="Kirk Modules - Option Documentation" \
+          --metadata date="$(date -u '+%Y-%m-%d - %H:%M:%S %Z')" \
+          --css=style.css \
+          -V lang=en \
+          -V --mathjax \
+          -f markdown+smart \
+          -o $out/"$filename_no_ext".html \
+          "$tmpdir"/done.md
+      }
+
+      # Generate nixos md docs
+      cat ${optionsDocNixos.optionsCommonMark} | tail -n +58 >> "$tmpdir"/nixos.md
+      # Generate home-manager md docs
+      cat ${optionsDocHome.optionsCommonMark} | tail -n +58 >> "$tmpdir"/home.md
+
+      buildpandoc "$tmpdir"/nixos.md
+      buildpandoc "$tmpdir"/home.md
+
+        pandoc \
+          --standalone \
+          --highlight-style docs/styling/gruvbox.theme \
+          --metadata title="Kirk Modules - Option Documentation" \
+          --metadata date="$(date -u '+%Y-%m-%d - %H:%M:%S %Z')" \
+          --css=style.css \
+          -V lang=en \
+          -V --mathjax \
+          -f markdown+smart \
+          -o $out/index.html \
+          docs/index.md
+    '';
+  }
+
+
+
